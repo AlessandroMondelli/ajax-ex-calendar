aa694f9bd4348c7d729b30406313ab3e4f8326a3
Alessandro_Mondelli <alexs98@hotmail.it>
2020-01-10 11:51:39 +0100

    Aggiunta grafica e pulsanti per mese successivo/precedente

--- main.js
+++ main.js
@@ -1,17 +1,74 @@
 $(document).ready(function() {
 
-    var firstDay = '2018-01-01';
-    var firstMoment = moment(firstDay);
-    console.log(firstMoment);
+    //Preparo template Handlebars
+    var source = $("#calendario-template").html();
+    //Preparo template con funzione
+    var template = Handlebars.compile(source);
 
-    var days = firstMoment.daysInMonth()
-    console.log(days);
+    var firstDay = '2018-01-01'; //Data iniziale
+    var lastDay = '2018-12-01'; //Data finale
+    var momentData = moment(firstDay);
 
-    var month = firstMoment.format('MMMM')
-    console.log(month);
+    getMonth(); //Funzione che crea il primo mese
 
-    for (var i = 0; i < days; i++) {
-        var htmlCode = "<li>" + (i + 1)  + "</li>";
-        $(".month").append(htmlCode);
+    $(".arrow.right").click(function() { //Al click su freccia destra
+            momentData.add(1,'month'); //Aggiungo un mese
+            $(".month-name,.month").empty(); //elimino mee precedente
+            getMonth(); //Prendo e stampo nuovo mese
+
+            if (momentData.isSameOrAfter(lastDay)) { //Controllo se è l'ultimo mese disponibile
+                $(this).prop('disabled',true); //Se non è disponibile disabilito freccia
+            }
+    });
+
+    $(".arrow.left").click(function() {
+            momentData.subtract(1,'month'); //Sottraggo un mese
+            $(".month-name,.month").empty(); //elimino mee precedente
+            getMonth(); //Prendo e stampo nuovo mese
+
+            if (momentData.isSameOrBefore(firstDay)) { //Controllo se è disponibile un mese precedente
+                $(this).prop('disabled',true); //Se non è disponibile disabilito freccia
+            }
+    });
+
+
+    //*** FUNZIONI ***//
+
+    function getMonth() { //Funzione che prende mese e giorni del mese
+        var days = momentData.daysInMonth(); //Giorni presenti nel mese richiesto
+        var month = momentData.format('MMMM'); //Prendo mese
+        var nMonth = momentData.month(); //Prendo numero del mese
+        appendMonth(days,month); //Passo dati a funzione che stamperà a schermo
+        getHoliday(nMonth); //Passo dati a funzoone che verifica festività
     }
+
+    function appendMonth(daysMonth,monthN) { //Funzione che stampa a schermo
+        for (var i = 0; i < daysMonth; i++) { //Ciclo per i giorni del mese
+            var placeholder = {
+                varDay : (i + 1) //Stampo giorno del mese
+            };
+            var finalHtml = template(placeholder); //Prendo template
+            $(".month").append(finalHtml); //Appendo in html
+        }
+        $(".month-name").append(monthN + " 2018"); //Appendo nome del mese
+    }
+
+    function getHoliday(monthSel) {
+        $.ajax ({ //Chiamata AJAX per recuperare dati film
+            'url' : 'https://flynn.boolean.careers/exercises/api/holidays',
+            'data' : {
+                'year' : 2018,
+                'month' : monthSel
+            },
+            'method' : 'GET', //Metodo GET
+            'success' : function(result) { //Caso funzionamento richiesta
+                var res = result.response;
+                console.log(res);
+            },
+            'error' : function() { //Caso di errore di caricamento
+                alert("Errore");
+            }
+        });
+    }
+
 });
